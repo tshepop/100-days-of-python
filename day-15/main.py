d2e0e381816b629ae95ae6291467f7be61d2bb32
Tshepo <potelo.tshepo@gmail.com>
2023-02-02 22:44:51 +0200

    Created display report function

---
+++ day-15/main.py
@@ -0,0 +1,31 @@
+from data import MENU, resources
+
+# Coffee machine project
+
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+money = 0
+profit = 0
+
+
+def displayReport():
+    global money
+
+    print("COFFEE MACHINE MAKER".center(26, "-"))
+    print("Available resources:")
+    print()
+
+    water = f"{resources.setdefault('water', 0)}ml".rjust(10, " ")
+    milk = f"{resources.setdefault('milk', 0)}ml".rjust(10, " ")
+    coffee = f"{resources.setdefault('coffee', 0)}g".rjust(10, " ")
+    money = f"${money}".rjust(10, " ")
+
+    print("Water ".ljust(15, ".") + water)
+    print("Milk ".ljust(15, ".") + milk)
+    print("Coffee ".ljust(15, ".") + coffee)
+    print("Cost ".ljust(15, ".") + money)
