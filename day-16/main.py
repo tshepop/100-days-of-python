d1f4bbbea640f34639259984f032e8858e2dfcf1
Tshepo <potelo.tshepo@gmail.com>
2023-02-06 18:18:00 +0200

    create oop main file, and import classes

---
+++ day-16/main.py
@@ -0,0 +1,7 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+drink_resources = CoffeeMaker()
+drink_cost = MoneyMachine()
+menu = Menu()
