0044afedbe9c2a559bd33d1be9154fff26b708bd
Tshepo <potelo.tshepo@gmail.com>
2023-05-29 17:24:58 +0200

    Turtle draws random color dots on screen

--- day-18/main.py
+++ day-18/main.py
@@ -16,3 +16,31 @@ for color in colors:
     color_spot.append((color.rgb[0], color.rgb[1], color.rgb[2]))
 
 # print(color_spot)
+
+# turtle default position
+# print(paint.pos())
+
+paint.penup()
+paint.setpos(-200, 200)
+paint.hideturtle()
+
+
+def draw():
+    """draw dots of size 20 pixels, in random colors"""
+    paint.dot(20, random.choice(color_spot))
+
+
+row_count = 10
+
+for row in range(10):
+    for col in range(10):
+        draw()
+        paint.fd(50)
+
+    paint.back(50 * row_count)
+
+    paint.right(90)
+    paint.fd(50)
+    paint.left(50)
+
+screen.exitonclick()
