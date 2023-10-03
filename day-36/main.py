b3d3e9296ecb14398f6202ba1012c75aeabf6b95
Tshepo <potelo.tshepo@gmail.com>
2023-10-03 14:54:05 +0200

    Retrieved data from the alphavantage api, and also retrieved news about the stock/company news using the api from newsapi.org/

--- day-36/main.py
+++ day-36/main.py
@@ -27,3 +27,61 @@ NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 # url = STOCK_ENDPOINT+query_str
 # r = requests.get(url)
 # data = r.json()
+
+data = {"function": "TIME_SERIES_DAILY",
+        "symbol": STOCK_NAME,
+        "apikey": MY_ALPHA_API
+        }
+
+response = requests.get(STOCK_ENDPOINT, params=data)
+time_series_data = response.json()["Time Series (Daily)"]
+# print(time_series_data)
+
+
+# TODO 1. - Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries. e.g. [new_value for (key, value) in dictionary.items()]
+
+# yesterday_stock_close = [value for (key, value) in time_series_data.items()]
+# print(yesterday_stock_close[0]["4. close"])
+# before_yesterday_close = [value for (key, value) in time_series_data.items()]
+# print(before_yesterday_close[1]["4. close"])
+
+
+for index, (key, value) in enumerate(time_series_data.items()):
+    # print(index, value)
+    if index == 0:
+        yesterday_stock_close = float(value["4. close"])
+        print("Yesterday's Close:", yesterday_stock_close)
+
+    # TODO 2. - Get the day before yesterday's closing stock price
+    if index == 1:
+        before_yesterday_close = float(value["4. close"])
+        print("Day Before Yesterday Close:", before_yesterday_close)
+
+
+# TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
+stock_price_pct = abs(before_yesterday_close -
+                      yesterday_stock_close) / yesterday_stock_close
+
+# TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
+stock_price_pct = round(float(stock_price_pct) * 100, 2)
+# or
+# print(f"Stock Percent Movement: {stock_price_pct:.2%}")
+
+
+# TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
+
+# STEP 2: https://newsapi.org/
+# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
+# if stock_price_pct < 5:
+# print("Get News")
+
+news_data = {"q": COMPANY_NAME,
+             "searchIn": ['title', 'description', 'content'],
+             "apiKey": MY_NEWS_API,
+             }
+
+res = requests.get(NEWS_ENDPOINT, params=news_data)
+# print(res.url)
+# TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
+company_news = res.json()["articles"]
+# print(company_news)
