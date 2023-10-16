1e40904431be47bc0c0ef10451dd1aa5ce633b1a
Tshepo <potelo.tshepo@gmail.com>
2023-10-16 20:26:00 +0200

    Day-38 main and config files created

---
+++ day-38/main.py
@@ -0,0 +1,21 @@
+import os
+
+import requests
+from datetime import datetime
+
+app_ID = os.environ.get("APP_ID")
+api_key = os.environ.get("API_KEY")
+
+EXERCISE_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
+ADD_ROW_ENDPOINT = "https://api.sheety.co/5ba0f577241a78c47f8e183661d59789/workoutTracking/sheet1"
+
+headers = {
+    "Authorization": os.environ.get("AUTH"),
+    "x-app-id": app_ID,
+    "x-app-key": api_key,
+    "Content-Type": "application/json",
+    "x-remote-user-id": "0"
+}
+
+# request input from user in a natural language format
+user_input = input("Tell me which exercises you did? ")
