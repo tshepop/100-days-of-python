0fe1160dc8048e054bba1b93f44f1dcb9523da98
Tshepo <potelo.tshepo@gmail.com>
2023-10-16 23:28:33 +0200

    Fixed problem of hiding secret keys

--- day-38/main.py
+++ day-38/main.py
@@ -1,16 +1,17 @@
-import os
-
+# import os
+import config
 import requests
 from datetime import datetime
 
-app_ID = os.environ.get("APP_ID")
-api_key = os.environ.get("API_KEY")
+app_ID = config.APP_ID
+api_key = config.API_KEY
+auth = config.AUTH
 
 EXERCISE_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
 ADD_ROW_ENDPOINT = "https://api.sheety.co/5ba0f577241a78c47f8e183661d59789/workoutTracking/sheet1"
 
 headers = {
-    "Authorization": os.environ.get("AUTH"),
+    "Authorization": auth,
     "x-app-id": app_ID,
     "x-app-key": api_key,
     "Content-Type": "application/json",
@@ -30,6 +31,7 @@ response = requests.post(url=EXERCISE_ENDPOINT,
 
 # print(response.text)
 exercises = response.json()["exercises"]
+# print(exercises)
 
 # data_list = [(value["user_input"], value["duration_min"], value["nf_calories"])
 #             for value in exercises]
