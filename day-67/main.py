c973c317f73b7d45538eb457782a56244d22b841
tshepo <potelo.tshepo@gmail.com>
2024-09-07 19:01:44 +0200

    Retrieve a blog post from the database based on id

--- day-67/main.py
+++ day-67/main.py
@@ -62,13 +62,11 @@ def get_all_posts():
 
     return render_template("index.html", all_posts=posts)
 
-# TODO: Add a route so that you can click on individual posts.
 
-
-@app.route('/')
+@app.route('/post/<int:post_id>', methods=['GET', 'POST'])
 def show_post(post_id):
-    # TODO: Retrieve a BlogPost from the database based on the post_id
-    requested_post = "Grab the post from your database"
+
+    requested_post = db.get_or_404(BlogPost, ident=post_id)
     return render_template("post.html", post=requested_post)
 
 
