6eb9ae85e8211db5a608490b9547398856042799
tshepo <potelo.tshepo@gmail.com>
2024-09-26 13:50:32 +0200

    Create register route.

--- day-68/main.py
+++ day-68/main.py
@@ -4,13 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+import config
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret-key-goes-here'
+app.config['SECRET_KEY'] = config.SECRET_KEY
+
 
 # CREATE DATABASE
-
-
 class Base(DeclarativeBase):
     pass
 
@@ -19,9 +19,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
+# config login manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
 # CREATE TABLE IN DB
-
-
 class User(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     email: Mapped[str] = mapped_column(String(100), unique=True)
@@ -33,13 +36,51 @@ with app.app_context():
     db.create_all()
 
 
+@login_manager.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
+
+
 @app.route('/')
 def home():
     return render_template("index.html")
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
+
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        error = None
+
+        user = db.session.execute(
+            db.select(User).where(User.email == email)).scalar()
+
+        if user:
+            error = "You've already signed up with that email, Please log in!"
+
+        new_user = User(
+            name=name,
+            email=email,
+            password=generate_password_hash(
+                password, method='pbkdf2:sha256', salt_length=8)
+        )
+
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+        except Exception:
+            flash(error)
+            return redirect(url_for("login"))
+        else:
+            login_user(new_user)
+            return redirect(url_for("secrets"))
+
     return render_template("register.html")
 
 
