3e9f352a534054ec29f45da0ede47750b8aff088
tshepo <potelo.tshepo@gmail.com>
2024-10-01 12:47:31 +0200

    Create user table

--- day-69/main.py
+++ day-69/main.py
@@ -61,6 +61,13 @@ class BlogPost(db.Model):
 
 
 # TODO: Create a User table for all your registered users.
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(String(100), unique=True)
+    password: Mapped[str] = mapped_column(String(100))
+    name: Mapped[str] = mapped_column(String(100))
 
 
 with app.app_context():
