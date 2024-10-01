94de8c9b4ff422ab9a8b6b96f8d42e482f977670
tshepo <potelo.tshepo@gmail.com>
2024-10-01 14:55:25 +0200

    Refactor code, remove redundant lines of code

--- day-69/main.py
+++ day-69/main.py
@@ -3,7 +3,6 @@ from flask import Flask, abort, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
 from flask_ckeditor.utils import cleanify
-from flask_gravatar import Gravatar
 from flask_login import UserMixin, login_required, login_user, LoginManager, current_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
@@ -14,18 +13,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 import config
 
-'''
-Make sure the required packages are installed: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from the requirements.txt for this project.
-'''
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = config.SECRET_KEY
